b73fc8391b31bd666b0d6d57a69910a581430c82
Trent.Stork <trent.stork1991@gmail.com>
2025-04-23 15:56:09 -0500

    player rotation left and right

--- main.py
+++ main.py
@@ -28,10 +28,14 @@ def main():
 		for event in pygame.event.get():
    		 	if event.type == pygame.QUIT:
         			return
+
+		dt = clock.tick(60) / 1000
+		player.update(dt)
+
 		screen.fill("black")
 		player.draw(screen)
 		pygame.display.flip()
-		dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 #imports
 import pygame
-from constants import PLAYER_RADIUS
+from constants import *
 from circleshape import CircleShape
 
 
@@ -8,6 +8,7 @@ class Player(CircleShape):
 	def __init__(self, x, y):
 		super().__init__(x,y, PLAYER_RADIUS)
 		self.rotation = 0
+
 
 	# in the player class
 	def triangle(self):
@@ -21,3 +22,16 @@ class Player(CircleShape):
 	# draw player shape
 	def draw(self, screen):
     		pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+	# player rotation
+	def rotate(self, dt):
+		self.rotation += PLAYER_TURN_SPEED * dt
+
+	# player update location 
+	def update(self, dt):
+		keys = pygame.key.get_pressed()
+
+		if keys[pygame.K_a]:
+			self.rotate(-dt)
+		if keys[pygame.K_d]:
+			self.rotate(dt)
