52300aec5ab79d64d011f507d9492a668188f07d
Trent.Stork <trent.stork1991@gmail.com>
2025-04-23 14:45:49 -0500

    circleshape and fps

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ def main():
 	#Create Screen
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
 
+	#Game clock-60fps
+	clock = pygame.time.Clock() 
+	dt = 0
+
 	#game loop
 	while True:
 		for event in pygame.event.get():
@@ -20,7 +24,7 @@ def main():
         			return
 		screen.fill("black")
 		pygame.display.flip()
-
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
