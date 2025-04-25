6ebf8b6fcca9db0474f02793d6c90a270d1b25dc
Trent.Stork <trent.stork1991@gmail.com>
2025-04-25 15:39:24 -0500

    gate down shooting speed to 0.3

--- player.py
+++ player.py
@@ -8,7 +8,7 @@ class Player(CircleShape):
 	def __init__(self, x, y):
 		super().__init__(x,y, PLAYER_RADIUS)
 		self.rotation = 0
-
+		self.shoot_timer = 0
 
 	# in the player class
 	def triangle(self):
@@ -34,14 +34,19 @@ class Player(CircleShape):
 
 	#player shooting
 	def shoot(self, shots_group):
+		if self.shoot_timer > 0:
+			return
 		new_shot = Shot(self.position.x, self.position.y)
 		shot_direction = pygame.Vector2(0,-1)
 		shot_direction = shot_direction.rotate(self.rotation)
 		new_shot.velocity = shot_direction * PLAYER_SHOOT_SPEED
 		shots_group.add(new_shot)
+		self.shoot_timer = PLAYER_SHOOT_COOLDOWN
 
 	# player update location 
 	def update(self, dt):
+		self.shoot_timer -= dt 
+		self.shoot_timer = max(self.shoot_timer, 0)
 		keys = pygame.key.get_pressed()
 
 		if keys[pygame.K_a]:
